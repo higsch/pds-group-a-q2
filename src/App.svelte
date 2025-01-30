48bcf99b8dcbf4e181f92bd2ece6ff02ebecefb1
Matthias Stahl <matthias.stahl87@web.de>
2025-01-30 14:46:28 +0100

    update

--- src/App.svelte
+++ src/App.svelte
@@ -20,14 +20,10 @@
             extraBox2.style.display = "none";
         }
     }
-
-    function checkAnswer(answer) {
-        selectedAnswer = answer; // Store the selected answer
-    }
 </script>
 
 <div class="text-box2">
-    <p>Wie viele PKW verursachen in <b>24 Stunden</b> genauso viel <b>Kohlendioxid </b> wie ein Kreuzfahrtschiff in derselben Zeit?</p>
+    <p>Wie viele PKW verursachen in <b>24 Stunden</b> genauso viel <b>Kohlendioxid</b> wie ein Kreuzfahrtschiff in derselben Zeit?</p>
 </div>
 
 <div class="slider2">
@@ -41,33 +37,22 @@
         on:keyup={checkGuess2}
         bind:value={value2}
     />
-    <label for="slider2"> {value2} </label>
+    <label for="slider2">{value2} Tsd.</label>
 
     <div class="pictograms">
         {#each Array(Math.floor(value2 / 1000)) as _}
             <img src="car4.png" alt="Car" width="20" />
         {/each}
-    
+    </div>
 
-  </div> 
-
- 
-
-    <p style="font-size: 12px; margin-top: 12px;">
-
- 
-     Ein Auto = 1 Millionen PKW
-     
-    </p>
-
-
+    <p style="font-size: 13px; margin-top: 12px;">Ein Auto = 1.000 PKW</p>
 </div>
 
 <div id="result-2" class="result-box-2"></div>
 
 <div id="extra-box2" class="extra-box2" style="display: none;">
     <p>
-        Ein Kreuzfahrtschiff stößt also am Tag sehr viel mehr Schadstoffe aus als ein Auto.
+Ein Kreuzfahrschiff stößt in 24 Stunden etwa so viel Kohlendioxid aus, wie <b> 83.640 PKW </b> in derselben Zeit. Da PKW andere Kraftstoffe als Schiffe nutzen, setzen sie weniger Schwefeldioxid und Stickoxide, aber <b> mehr Kohlendioxid </b> frei. Trotzdem ist der CO₂-Ausstoß eines Kreuzfahrtschiffs um ein Vielfaches höher. 
     </p>
 </div>
 
@@ -76,12 +61,11 @@
         background-color: #efe0cd; /* Beige background */
         margin: 0; /* Remove default body margin */
         color: #427fb7; /* Blue text */
-        font-family; PT Serif:
         font-size: 18px;
+        font-family: PT Serif;
     }
-    
+
     div {
-        
         padding: 10px;
     }
 
@@ -94,6 +78,13 @@
     .slider2 {
         margin: 30px;
         color: #427fb7; /* Blue text on slider */
+        text-align: center;
+    }
+
+    input[type="range"] {
+        width: 50%; /* Make the slider smaller */
+        display: block; /* Center the slider */
+        margin: 10px auto; /* Center alignment using auto margins */
     }
 
     .result-box-2 {
@@ -113,14 +104,10 @@
         margin: 20px 30px;
     }
 
-    input[type="range"] {
-        margin-top: 10px;
-        width: 100%;
-    }
-
     .pictograms {
         display: flex;
         flex-wrap: wrap;
+        justify-content: center; /* Center the pictograms */
     }
 
     .pictograms img {
